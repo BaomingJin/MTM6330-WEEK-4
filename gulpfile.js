434dba0abc17b6a7e7d260e0f2519d1c9114a52d
BAOMING JIN <jin00055@algonquinlive.com>
2019-01-28 11:49:41 -0500

    “Add watch task to GulpSass compiler.

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,14 @@ gulp.task('sass', function () {
     .pipe(sass())
     .pipe(gulp.dest('css'))
 })
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+gulp.task('sass',function(){
+  return GULP
+  .src('scss/**/*.scss')
+  pipe(sass())
+  .pipe(gulp.dest('css'))
+})
+GULP.task('default',function(){
+  gulp.watch('scss/**/*.scss',gulp.series('sass'))
+})
