19956c0b3266ac5dd79b6b11d14882878fafc2c9
BAOMING JIN <jin00055@algonquinlive.com>
2019-02-04 09:48:36 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,4 @@
 const gulp = require('gulp')
-
-const sass = require('gulp-sass')
-
-gulp.task('sass', function () {
-  return gulp
-    .src('scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('css'))
-})
-const gulp = require('gulp')
 const sass = require('gulp-sass')
 gulp.task('sass',function(){
   return gulp
